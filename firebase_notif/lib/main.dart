3143334d6de05f6f5fa5c660f222f7d47803d91e
alfanmahdi <amarthchannel@gmail.com>
2025-05-19 23:21:42 +0700

    fix: notif initialize

--- firebase_notif/lib/main.dart
+++ firebase_notif/lib/main.dart
@@ -5,9 +5,11 @@ import 'package:firebase_notif/screens/second.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_notif/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await NotificationService().initializeNotification();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
